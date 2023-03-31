8bad42b98de1b7ebd9585f194c113271e091c93e
root <root@SHADOW-0GFON6US.localdomain>
2023-03-31 17:12:11 +0200

    create page Infos

---
+++ src/Pages/Infos.jsx
@@ -0,0 +1,11 @@
+const Infos = () => {
+    return (
+        <div className="infos" id="infos">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero quos dolor doloremque quia.
+                Eveniet impedit nulla odit architecto
+                non nisi tempore ea debitis iste molestiae, doloribus, magnam nam repellat asperiores.</p>
+        </div>
+    )
+}
+
+export default Infos;
