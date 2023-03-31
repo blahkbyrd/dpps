3d045a45f98c61a1df069e1a9f49c7effac49a0d
root <root@SHADOW-0GFON6US.localdomain>
2023-03-31 17:08:34 +0200

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Footer from "./Components/Footers/Footer";
 import Header from "./Components/Headers/Header";
+import Layout from "./Components/Layouts/Layout";
 import Home from "./Pages/Home";
 
+import "./css/style.css";
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
       <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Layout/>}/>
       </Routes>
       </BrowserRouter>
       <Footer/>
