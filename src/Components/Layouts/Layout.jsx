52a5b9bcfe9adfca43ec0a51892bdad21c4e42b8
root <root@SHADOW-0GFON6US.localdomain>
2023-03-31 17:09:17 +0200

    add layout 	implement scrolling navigation

---
+++ src/Components/Layouts/Layout.jsx
@@ -0,0 +1,17 @@
+import Home from "../../Pages/Home"
+import Contact from "../../Pages/Contact"
+import Infos from "../../Pages/Infos"
+import Planning from "../../Pages/Planinng"
+
+const Layout = () => {
+    return(
+        <>
+            <Home />
+            <Infos />
+            <Planning/>
+            <Contact/>
+        </>
+    )
+}
+
+export default Layout
--- src/Components/Utils/Navbar.jsx
+++ src/Components/Utils/Navbar.jsx
@@ -1,5 +1,8 @@
 
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import {Link} from "react-scroll";
+
+// TODO : add easing https://www.npmjs.com/package/react-scroll
 
 function Navbar() {
     const location = useLocation();
@@ -7,9 +10,11 @@ function Navbar() {
     return (
         <div className='navbar'>
             <ul>
-                <li className={location.pathname === "/" ? "active" : "inactive"}><Link to="/">accueil</Link></li>
-                <li className={location.pathname === "/contact" ? "active" : "inactive"}><Link to="/contact">contact</Link></li>
-                <li className={location.pathname === "/planning" ? "active" : "inactive"}><Link to="/planning">prendre un rdv</Link></li>
+                <li className={location.pathname === "/" ? "active" : "inactive"}><Link to="home" spy={true} smooth={true}>accueil</Link></li>
+                <li className={location.pathname === "/" ? "active" : "inactive"}><Link to="infos" spy={true} smooth={true}>infos</Link></li>
+                <li className={location.pathname === "/" ? "active" : "inactive"}><Link to="planning" spy={true} smooth={true}>prendre rdv</Link></li>
+                <li className={location.pathname === "/" ? "active" : "inactive"}><Link to="contact" spy={true} smooth={true}>contact</Link></li>
+
             </ul>
 
         </div>
