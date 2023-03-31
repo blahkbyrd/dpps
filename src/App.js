faf37a7f51cfa981ecca1c80121c39d424f3332e
root <root@SHADOW-0GFON6US.localdomain>
2023-03-31 17:32:38 +0200

    styling images

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Footer from "./Components/Footers/Footer";
 import Header from "./Components/Headers/Header";
 import Layout from "./Components/Layouts/Layout";
-import Home from "./Pages/Home";
+
 
 import "./css/style.css";
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,8 +8,24 @@ import breizh from "../Assets/images/breizh.jpg";
 import macchiato from "../Assets/images/macchiato.jpg";
 
 const Home = () => {
+    const images_source = [{
+        id:0,
+        alternative:"chien",
+        src: eddu
+    },
+    {
+        id:1,
+        alternative:"lapin",
+        src:breizh
+    },
+    {
+        id:2,
+        alternative:"lapin",
+        src:macchiato
+    }
+    ]
     const content = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, aut, voluptatum, dignissimos porro corporis adipisci deserunt distinctio modi vel odio tempora unde culpa dolorem voluptatem cumque? Excepturi optio quia minus!"
-    
+
     const action_1 = () => {
         console.log("click !");
     }
@@ -17,22 +33,23 @@ const Home = () => {
     const action_2 = () => {
         console.log("Another button");
     }
-    
+
     return (
         <div className="home" id="home">
-            <aside>
-                <ul>
-                    <li><Imagecard source={eddu} alternative="chien"/></li>
-                    <li><Imagecard source={breizh} alternative="lapin"/></li>
-                    <li><Imagecard source={macchiato} alternative="lapin"/></li>
+            <aside className="image-container" >
+                <ul>{
+                    images_source.map(element =>{
+                       return (<li key={element.id}><Imagecard source={element.src} alternative={element.alternative}/></li>)
+                    })
+                }
                 </ul>
             </aside>
             <main>
-            <Textcard title="Titre" content={content} />
-            <div className="button-container">
-                <Primarybutton action={action_1} content="+ d'infos"/>
-                <Secondarybutton action={action_2} content="prendre rdv"/>
-            </div>
+                <Textcard title="Titre" content={content} />
+                <div className="button-container">
+                    <Primarybutton action={action_1} content="+ d'infos" />
+                    <Secondarybutton action={action_2} content="prendre rdv" />
+                </div>
             </main>
         </div>
     )
